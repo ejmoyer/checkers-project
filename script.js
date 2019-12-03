3562cbdca607e978df3f99b81018ea3d643bb1c6
Eric Moyer <ejmoyer3@comcast.net>
2019-12-03 14:06:03 -0500

    Added loop to make div grid, also added defer to the script tag in index.html.

---
+++ script.js
@@ -0,0 +1,8 @@
+for (let x = 0; x < 8; x++) {
+  for (let y = 0; y < 8; y++) {
+  let columnDiv = document.createElement('div')
+  document.body.appendChild(columnDiv);
+  }
+  let rowDiv = document.createElement('div')
+  document.body.appendChild(rowDiv);
+}
