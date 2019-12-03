cf2dd7807e2335d2a69bc63e76cabc7f3d89f00d
Eric Moyer <ejmoyer3@comcast.net>
2019-12-03 16:18:39 -0500

    Created board, added CSS to give board colors, created an article to hold the board.

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
+let board = document.getElementsByClassName('board')[0];
+
 for (let x = 0; x < 8; x++) {
-  for (let y = 0; y < 8; y++) {
-  let columnDiv = document.createElement('div')
-  document.body.appendChild(columnDiv);
-  }
   let rowDiv = document.createElement('div')
-  document.body.appendChild(rowDiv);
+  rowDiv.setAttribute('class', 'space')
+  board.appendChild(rowDiv);
+  for (let y = 0; y < 7; y++) {
+    let columnDiv = document.createElement('div')
+    columnDiv.setAttribute('class', 'space')
+    if ((x + y) % 2 == 0) {
+      columnDiv.style.background = "red";
+      rowDiv.style.background = "black";
+    } else {
+      columnDiv.style.background = "black";
+      rowDiv.style.background = "red";
+    }
+    board.appendChild(columnDiv);
+  }
 }
