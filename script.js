510540d3c85f40b883118a2d9fa34e57132ab46f
Eric Moyer <ejmoyer3@comcast.net>
2019-12-04 13:14:44 -0500

    Changed .space color to white to see placeholder pieces easier. Added code to put pieces on the board and put the whole beginning code into a function.

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 let board = document.getElementsByClassName('board')[0];
-
-for (let x = 0; x < 8; x++) {
+let playerOne = "BLUE";
+let playerTwo = "GREEN";
+const startGame = () => {
+for (let y = 0; y < 8; y++) {
   let rowDiv = document.createElement('div')
   rowDiv.setAttribute('class', 'space')
   board.appendChild(rowDiv);
-  for (let y = 0; y < 7; y++) {
+  for (let x = 0; x < 7; x++) {
     let columnDiv = document.createElement('div')
     columnDiv.setAttribute('class', 'space')
     if ((x + y) % 2 == 0) {
@@ -13,7 +15,18 @@ for (let x = 0; x < 8; x++) {
     } else {
       columnDiv.style.background = "black";
       rowDiv.style.background = "red";
+    } if ((((y == 0) || (y == 2)) && (x % 2 == 0)) || ((y == 1) && (x % 2 != 0))) {
+      columnDiv.appendChild(document.createTextNode(playerOne))
+    } else if ((((y == 5) || (y == 7)) && (x % 2 == 0)) || ((y == 6) && (x % 2 != 0))) {
+      columnDiv.appendChild(document.createTextNode(playerTwo))
     }
     board.appendChild(columnDiv);
   }
+  if (y == 6) {
+  rowDiv.appendChild(document.createTextNode(playerTwo))
+} else if (y == 1) {
+  rowDiv.appendChild(document.createTextNode(playerOne))
 }
+}
+}
+startGame();
