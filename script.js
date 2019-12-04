7dbdc6361980c93bbce9b24f55ea09955be2687a
Eric Moyer <ejmoyer3@comcast.net>
2019-12-04 13:38:12 -0500

    Gave player one and two pieces respective classes so they are easier to select later.

--- script.js
+++ script.js
@@ -18,10 +18,12 @@ for (let y = 0; y < 8; y++) {
     } if ((((y == 0) || (y == 2)) && (x % 2 == 0)) || ((y == 1) && (x % 2 != 0))) {
       let playerOnePiece = document.createElement('p');
       playerOnePiece.appendChild(document.createTextNode(playerOne))
+      playerOnePiece.setAttribute('class', 'playerOne')
       columnDiv.appendChild(playerOnePiece)
     } else if ((((y == 5) || (y == 7)) && (x % 2 != 0)) || ((y == 6) && (x % 2 == 0))) {
       let playerTwoPiece = document.createElement('p');
       playerTwoPiece.appendChild(document.createTextNode(playerTwo))
+      playerTwoPiece.setAttribute('class', 'playerTwo')
       columnDiv.appendChild(playerTwoPiece);
     }
     board.appendChild(columnDiv);
@@ -29,10 +31,12 @@ for (let y = 0; y < 8; y++) {
   if ((y == 5) || (y == 7)) {
   let playerTwoPiece = document.createElement('p');
   playerTwoPiece.appendChild(document.createTextNode(playerTwo))
+  playerTwoPiece.setAttribute('class', 'playerTwo')
   rowDiv.appendChild(playerTwoPiece);
 } else if (y == 1) {
   let playerOnePiece = document.createElement('p');
   playerOnePiece.appendChild(document.createTextNode(playerOne))
+  playerOnePiece.setAttribute('class', 'playerOne')
   rowDiv.appendChild(playerOnePiece);
 }
 }
