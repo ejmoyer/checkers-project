0bf9cb5b759b2b79db7b7b4057de527144a54494
Andrew Scholly <SATmaster728@gmail.com>
2019-12-05 12:55:44 -0500

    added piece movement

--- script.js
+++ script.js
@@ -1,44 +1,64 @@
 let board = document.getElementsByClassName('board')[0];
-let playerOne = "BLUE";
-let playerTwo = "GREEN";
 const startGame = () => {
-for (let y = 0; y < 8; y++) {
-  let rowDiv = document.createElement('div')
-  rowDiv.setAttribute('class', 'space')
-  board.appendChild(rowDiv);
-  for (let x = 0; x < 7; x++) {
-    let columnDiv = document.createElement('div')
-    columnDiv.setAttribute('class', 'space')
-    if ((x + y) % 2 != 0) {
-      columnDiv.style.background = "red";
-      rowDiv.style.background = "black";
-    } else {
-      columnDiv.style.background = "black";
-      rowDiv.style.background = "red";
-    } if ((((y == 0) || (y == 2)) && (x % 2 == 0)) || ((y == 1) && (x % 2 != 0))) {
-      let playerOnePiece = document.createElement('p');
-      playerOnePiece.appendChild(document.createTextNode(playerOne))
-      playerOnePiece.setAttribute('class', 'playerOne')
-      columnDiv.appendChild(playerOnePiece)
-    } else if ((((y == 5) || (y == 7)) && (x % 2 != 0)) || ((y == 6) && (x % 2 == 0))) {
-      let playerTwoPiece = document.createElement('p');
-      playerTwoPiece.appendChild(document.createTextNode(playerTwo))
-      playerTwoPiece.setAttribute('class', 'playerTwo')
-      columnDiv.appendChild(playerTwoPiece);
+  for (let y = 0; y < 8; y++) {
+    let rowDiv = document.createElement('div')
+    rowDiv.setAttribute('class', 'space')
+    const movepiece = event => {
+      if (document.getElementById('clicked') != null) {
+        //if (){
+        //if (x is plus 7 or 9 or x is minus 7 or 9)
+        //do the code below
+        event.target.setAttribute('id', 'secondclick');
+        document.getElementById('secondclick').appendChild(document.getElementById('clicked'));
+        document.getElementById('clicked').removeAttribute('id');
+        document.getElementById('secondclick').removeAttribute('id');
+        //else ()
+        //return some sort of error
+      //}
+      //  else {
+      //  alert ('Not a legal move!');
+      //}
+    }else if (event.target.nodeName != 'DIV'){
+        event.target.setAttribute('id', 'clicked');
+      }
+    };
+    rowDiv.addEventListener('click', movepiece);
+    board.appendChild(rowDiv);
+    for (let x = 0; x < 7; x++) {
+      let columnDiv = document.createElement('div')
+      columnDiv.setAttribute('class', 'space')
+      if ((x + y) % 2 != 0) {
+        columnDiv.style.background = "red";
+        rowDiv.style.background = "black";
+      } else {
+        columnDiv.style.background = "black";
+        rowDiv.style.background = "red";
+      }
+      if ((((y == 0) || (y == 2)) && (x % 2 == 0)) || ((y == 1) && (x % 2 != 0))) {
+        let playerOnePiece = document.createElement('p');
+        playerOnePiece.appendChild(document.createTextNode("BLUE"))
+        playerOnePiece.setAttribute('class', 'playerOne')
+        columnDiv.appendChild(playerOnePiece)
+      } else if ((((y == 5) || (y == 7)) && (x % 2 != 0)) || ((y == 6) && (x % 2 == 0))) {
+        let playerTwoPiece = document.createElement('p');
+        playerTwoPiece.appendChild(document.createTextNode("GREEN"))
+        playerTwoPiece.setAttribute('class', 'playerTwo')
+        columnDiv.appendChild(playerTwoPiece);
+      }
+      columnDiv.addEventListener('click', movepiece);
+      board.appendChild(columnDiv);
     }
-    board.appendChild(columnDiv);
+    if ((y == 5) || (y == 7)) {
+      let playerTwoPiece = document.createElement('p');
+      playerTwoPiece.appendChild(document.createTextNode("GREEN"))
+      playerTwoPiece.setAttribute('class', 'playerTwo')
+      rowDiv.appendChild(playerTwoPiece);
+    } else if (y == 1) {
+      let playerOnePiece = document.createElement('p');
+      playerOnePiece.appendChild(document.createTextNode("BlUE"))
+      playerOnePiece.setAttribute('class', 'playerOne')
+      rowDiv.appendChild(playerOnePiece);
+    }
   }
-  if ((y == 5) || (y == 7)) {
-  let playerTwoPiece = document.createElement('p');
-  playerTwoPiece.appendChild(document.createTextNode(playerTwo))
-  playerTwoPiece.setAttribute('class', 'playerTwo')
-  rowDiv.appendChild(playerTwoPiece);
-} else if (y == 1) {
-  let playerOnePiece = document.createElement('p');
-  playerOnePiece.appendChild(document.createTextNode(playerOne))
-  playerOnePiece.setAttribute('class', 'playerOne')
-  rowDiv.appendChild(playerOnePiece);
-}
-}
 }
 startGame();
