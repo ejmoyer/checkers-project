b8fbb23cf2fcfa454154b5f758911c1939074cf4
Eric Moyer <ejmoyer3@comcast.net>
2019-12-05 16:19:51 -0500

    Made it so the pieces are restricted from moving freely/too far.

--- script.js
+++ script.js
@@ -1,22 +1,34 @@
 let board = document.getElementsByClassName('board')[0];
 
+let firstX;
+let firstY;
+
 const createMovePiece = (x, y) => (event) => {
+
   if (document.getElementById('clicked') != null) {
     //create function (x, y) {returns () => console.log(x, y)}
-    event.target.appendChild(document.getElementById('clicked'));
-    document.getElementById('clicked').removeAttribute('id');
+    console.log(x, y)
+    if (x == (firstX + 1) && (y == (firstY - 1)) || (x == (firstX - 1)) && (y == (firstY - 1))) {
+      event.target.appendChild(document.getElementById('clicked'));
+      document.getElementById('clicked').removeAttribute('id');
+    }
     //  else {
     //  alert ('Not a legal move!');
     //}
   } else if (event.target.nodeName != 'DIV') {
-    console.log(x, y);
+    firstX = x;
+    firstY = y;
+    console.log(`X is ${x}, Y is ${y}`);
     event.target.setAttribute('id', 'clicked');
+
   }
 };
 
 const startGame = () => {
   for (let y = 0; y < 8; y++) {
+
     for (let x = 0; x < 8; x++) {
+
       let square = document.createElement('div')
       square.setAttribute('class', 'space')
 
